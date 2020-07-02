70c724f3e876a1828938e020b216fcfad4fcd7d7
Christine Rhee <rheec2016@gmail.com>
2020-07-02 15:19:28 +0900

    cloudinary logo

---
+++ _site/assets/js/navigation.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+  
+  // interaction
+
+  $('.navigation-trigger, .navigation a').click(function() {
+    $('body').toggleClass('sidebar-is-active');
+  });
+  
+  // smooth scrolling
+
+  $(function() {
+
+    $('a[href*=\\#]:not([href=\\#])').click(function() {
+
+      if (location.pathname.replace(/^\//,'') === this.pathname.replace(/^\//,'') && location.hostname === this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+
+        if (target.length) {
+          var targety = target.offset().top;
+          var realtargety = targety - 32;
+          $('html,body').animate({
+            scrollTop: realtargety
+          }, 500, 'swing');
+          return false;
+        }
+
+      }
+
+    });
+
+  });
+
+});
